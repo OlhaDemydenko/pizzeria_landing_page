d6fdbe2fd58099ef35258e0be8d388c17ef9ac27
Olha Demydenko <olhadem911@gmail.com>
2019-03-19 15:09:22 +0200

    menu-burger without close event

--- src/js/header.js
+++ src/js/header.js
@@ -1,7 +1,16 @@
 let menuBtn = document.getElementsByClassName('menu-btn')[0];
+let menuList = document.getElementsByClassName('top-nav_menu')[0];
+let nav = document.getElementById("menu-block");
+let logoCol = document.getElementsByClassName('menu-column')[0];
+let menuCol = document.getElementsByClassName('menu-column')[1];
 
 menuBtn.addEventListener('click', function(e){
 	e.preventDefault();
 	menuBtn.classList.toggle('menu-btn_active');
+	menuList.classList.toggle('drop-down');
+	nav.classList.toggle('nav_drop-down');
+	logoCol.classList.toggle('col-12');
+	menuCol.classList.toggle('col-12');
 })
+
 
