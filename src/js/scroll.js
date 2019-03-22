28e9404d85391ba3a92fb47e3acee8a8dac3f785
Olha Demydenko <olhadem911@gmail.com>
2019-03-22 12:44:12 +0200

    fixed menu and local fonts

--- src/js/scroll.js
+++ src/js/scroll.js
@@ -1,2 +1,14 @@
+let menu = document.getElementById("menu-block");
+let secondScreen = document.getElementById("aboutus-screen");
 
 
+window.onscroll = function() {
+	let interval = secondScreen.getBoundingClientRect().top;
+	let show = interval + window.pageYOffset;
+	if(menu.classList.contains('fixed') && interval >= 150){
+		menu.classList.remove('fixed');
+	}
+	else if (interval < 150){
+		menu.classList.add('fixed');
+	}
+};
